0e874ee180d46eacf0a0cbf21422793c824ba55b
unknown <milosmetlic89@gmail.com>
2023-01-11 17:53:14 +0100

    Quick fix

--- file_content.h
+++ file_content.h
@@ -3,7 +3,7 @@
 // string indexPage = "<?php \n\techo \"Hello World!\";  \n?>";
 string content[3] = {
 
-    "<?php \n//Env File\n?>",
+    "#Add environment variables",
     "/*Css*/\n\n\nbody\n{\n\tmargin: 0 auto;\n}\n",
     "<?php \n\techo \"Hello World!\";  \n?>"
 };
